36db690b4894f3cb8310c27ee172ae85ba69c8bd
veonliew <32752666+veonliew@users.noreply.github.com>
2019-01-08 05:29:43 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,20 +1,12 @@
-## step 1: Load data into R
 fullData <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?") 
 
-## step 2: Subset the data 
 data1 <- subset(fullData, Date %in% c("1/2/2007","2/2/2007"))
 
-## step 3: convert the date variable to Date class 
 data1$Date <- as.Date(data1$Date, format = "%d/%m/%Y")
 
-## step 4: convert dates and times to the POSIXct class
 data1$Datetime <- strptime(paste(data1$Date, data1$Time), "%Y-%m-%d %H:%M:%S")
-#option: Datetime <- paste(as.Date(data1$Date), data1$Time)
-data1$Datetime <- as.POSIXct(data1$Datetime) # or option: as.POSIXct(Datetime)
+data1$Datetime <- as.POSIXct(data1$Datetime) 
 
-## step 5: Creat plot 2
-png("plot2.png", width=480, height=480) # Open PNG device; create 'plot2.png' in my working diretory
-# Create a plot, annotate plot and send to the file
+png("plot2.png", width=480, height=480)
 with(data1, plot(Datetime, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
-#Option: with(data1, plot(Global_active_power~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
-dev.off() # Close the PNG file device
+dev.off() 
