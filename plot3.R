fed7cd03e407e6473e22664dcf2c0dced938a916
veonliew <32752666+veonliew@users.noreply.github.com>
2019-01-08 15:06:21 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,20 +1,13 @@
-## step 1: Load data into R
 fullData <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?") 
-
-## step 2: Subset the data 
+ 
 data1 <- subset(fullData, Date %in% c("1/2/2007","2/2/2007"))
 
-## step 3: convert the date variable to Date class 
 data1$Date <- as.Date(data1$Date, format = "%d/%m/%Y")
 
-## step 4: convert dates and times to the POSIXct class
 data1$Datetime <- strptime(paste(data1$Date, data1$Time), "%Y-%m-%d %H:%M:%S")
-#option: Datetime <- paste(as.Date(data1$Date), data1$Time)
-data1$Datetime <- as.POSIXct(data1$Datetime) # or option: as.POSIXct(Datetime)
+data1$Datetime <- as.POSIXct(data1$Datetime) 
 
-## step 5: Creat plot 3
-png("plot3.png", width=480, height=480) # Open PNG device; create 'plot3.png' in my working diretory
-# Create a plot, annotate plot and send to the file
+png("plot3.png", width=480, height=480) 
 with(data1, {
   plot(Datetime, Sub_metering_1, type="l",
        ylab="Enery sub metering", xlab="")
@@ -23,4 +16,4 @@ with(data1, {
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.off() # Close the PNG file device
+dev.off() 
